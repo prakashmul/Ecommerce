ff838ac8a27b04363dc16c2ddacf26a272f07fa7
prakashmul <100404694+prakashmul@users.noreply.github.com>
2024-05-30 14:31:00 +0545

    signin and contact page form validation

--- src/component/signin/signinpage.tsx
+++ src/component/signin/signinpage.tsx
@@ -1,55 +1,109 @@
 import { Link } from "react-router-dom"
+import *  as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup"
+import { useForm } from "react-hook-form";
+import { useCallback } from "react";
+import Button from "../reusable/button/button";
+
+interface ISigninForm {
+    email: string,
+    password: string
+}
 
 const SigninPage = () => {
-  return (
-    <div>
+    const signinValidation = yup.object().shape({
+        email: yup.string().email("Please enter a valid email address").required("Email address is required"),
+        password: yup
+            .string()
+            .required("Password is required")
+            .min(8, "Password must be at least 8 characters")
+            .matches(
+                /^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&#])[A-Za-z\d@$!%?&#]+$/,
+                "Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character",
+            ),
+    })
+
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<ISigninForm>({
+        resolver: yupResolver(signinValidation),
+    });
+
+    const onSignin = useCallback((values: ISigninForm) => {
+        console.log(values);
+    }, [])
+
+    return (
+        <div>
 
 
-    <section className="bg-gray-50 dark:bg-gray-900">
-        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
-            <div className="flex flex-col justify-center">
-                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
-                <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+            <section className="bg-gray-50 dark:bg-gray-900" onSubmit={handleSubmit(onSignin)}>
+                <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
+                    <div className="flex flex-col justify-center">
+                        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
+                        <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
 
-            </div>
-            <div>
-                <div className="w-full lg:max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow-xl dark:bg-gray-800">
-                    <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
-                        Sign in
-                    </h2>
-                    <form className="mt-8 space-y-6" action="#">
-                        <div>
-                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                    </div>
+                    <div>
+                        <div className="w-full lg:max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow-xl dark:bg-gray-800">
+                            <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
+                                Sign in
+                            </h2>
+                            <form className="mt-8 space-y-6" action="#">
+                                <div>
+                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                    <input type="email"
+                                        id="email"
+                                        {...register("email")}
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" />
+                                    {
+                                        errors.email &&
+                                        <span className="text-red-600">{errors.email.message}</span>
+                                    }
+                                </div>
+                                <div>
+                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                                    <input type="password"
+                                        {...register("password")}
+                                        id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                                    {
+                                        errors.password &&
+                                        <span className="text-red-600">{errors.password.message}</span>
+                                    }
+                                </div>
+                                <div className="flex items-start">
+                                    <div className="flex items-center h-5">
+                                        <input id="remember" aria-describedby="remember" name="remember" type="checkbox" className="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600" />
+                                    </div>
+                                    <div className="ms-3 text-sm">
+                                        <label className="font-medium text-gray-500 dark:text-gray-400">Remember this device</label>
+                                    </div>
+                                    <a href="#" className="ms-auto text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Lost Password?</a>
+                                </div>
+                                <Button
+                                    buttonType={'submit'}
+                                    buttonColor={{
+                                        primary: true,
+                                    }}>
+                                    Log in to your account
+                                </Button>
+                                <div className="text-sm font-medium text-gray-900 dark:text-white">
+                                    Not registered yet?
+                                    <Link to="/register">
+                                        <button className="text-blue-600 hover:underline dark:text-blue-500">Register</button>
+                                    </Link>
+                                </div>
+                            </form>
                         </div>
-                        <div>
-                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-                        </div>
-                        <div className="flex items-start">
-                            <div className="flex items-center h-5">
-                                <input id="remember" aria-describedby="remember" name="remember" type="checkbox" className="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600" required />
-                            </div>
-                            <div className="ms-3 text-sm">
-                                <label className="font-medium text-gray-500 dark:text-gray-400">Remember this device</label>
-                            </div>
-                            <a href="#" className="ms-auto text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Lost Password?</a>
-                        </div>
-                        <button type="submit" className="w-full px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
-                        <div className="text-sm font-medium text-gray-900 dark:text-white">
-                            Not registered yet? 
-                            <Link to="/register">
-                            <button className="text-blue-600 hover:underline dark:text-blue-500">Register</button>
-                            </Link>
-                        </div>
-                    </form>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </section>
+            </section>
 
-</div>
-  )
+        </div>
+    )
 }
 
 export default SigninPage
--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,27 +1,81 @@
-import React from 'react'
+import *  as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup"
+import { useCallback } from "react";
+import { useForm } from "react-hook-form";
+import Button from "../../component/reusable/button/button";
+
+interface IContactForm {
+  email: string,
+  subject: string,
+  message: string
+}
 
 const Contact = () => {
+  const contactValidation = yup.object().shape({
+    email: yup.string().email("Please enter a valid email address").required("Email address is required"),
+    subject: yup.string().required("subject is required"),
+    message: yup.string().required("Message is required"),
+  })
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<IContactForm>({
+    resolver: yupResolver(contactValidation),
+  });
+
+  const onRegister = useCallback((values: IContactForm) => {
+    console.log(values);
+  }, [])
+
   return (
     <div>
-      <form className="max-w-sm mx-auto border rounded-lg">
+      <form className="max-w-sm mx-auto border rounded-lg" onSubmit={handleSubmit(onRegister)}>
         <div className='m-5'>
           <div className="mb-5">
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-            <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+            <input type="email"
+             id="email"
+             {...register("email")}
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" />
+              {
+              errors.email &&
+              <span className="text-red-600">{errors.email.message}</span>
+            }
           </div>
           <div className="mb-5">
             <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900">Subject</label>
-            <input type="text" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+            <input type="text" 
+            {...register("subject")}
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
+            {
+              errors.subject &&
+              <span className="text-red-600">{errors.subject.message}</span>
+            }
           </div>
           <div className="mb-5">
             <label htmlFor="message" className="mb-2 text-sm font-medium text-gray-900">Your Message</label>
-            <textarea rows={7} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5" required />
+            <textarea rows={7} 
+            {...register("message")}
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5"/>
+            {
+              errors.message &&
+              <span className="text-red-600">{errors.message.message}</span>
+            }
           </div>
           <div>
           <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" >Upload file</label>
           <input className="block w-full text-sm mb-4 text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50" id="file_input" type="file" />
         </div>
-          <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Send Message</button>
+        <Button
+            buttonType={'submit'}
+            buttonColor={{
+              primary: true,
+            }}>
+            Send Message
+          </Button>
 
           <div className="text-sm font-medium text-gray-500 mt-4">
             <p>info@company.com</p>
