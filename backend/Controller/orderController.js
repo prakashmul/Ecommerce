eeb1c7debae752b659a620f4ba2e45f7147ae637
prakashmul <100404694+prakashmul@users.noreply.github.com>
2024-08-18 20:59:22 +0545

    delete order by order id

--- backend/Controller/orderController.js
+++ backend/Controller/orderController.js
@@ -1,22 +1,22 @@
-const OrderModel = require("../Model/orderModel")
+const OrderModel = require("../Model/orderModel");
+const mongoose = require('mongoose');
 
-exports.createOrder = async(req,res) => {
-    const {userId, productId, totalOrder}= req.body
+exports.createOrder = async (req, res) => {
+  const { userId, productId, totalOrder } = req.body;
 
-    const orderCreate = new OrderModel({
-        user: userId, 
-        product: productId,
-        totalOrder: totalOrder
-    })
-    
-    if(!orderCreate){
-        return res.status(400).json({error: "Failed to create order"})
-    }
+  const orderCreate = new OrderModel({
+    user: userId,
+    product: productId,
+    totalOrder: totalOrder,
+  });
 
-    orderCreate.save()
-    return res.status(200).json({message: "Order created successfully"})
-}
+  if (!orderCreate) {
+    return res.status(400).json({ error: "Failed to create order" });
+  }
 
+  orderCreate.save();
+  return res.status(200).json({ message: "Order created successfully" });
+};
 
 exports.getAllOrders = async (req, res) => {
   const { sortBy, limit, sortOrder } = req.query;
@@ -25,36 +25,49 @@ exports.getAllOrders = async (req, res) => {
   const total = limit ? limit : 100000000;
   const sort_order = sortOrder ? sortOrder : "asc";
 
-    const orders = await OrderModel.find()
-    .populate('user', 'userDetail email')
-    .populate('product')
+  const orders = await OrderModel.find()
+    .populate("user", "userDetail email")
+    .populate("product")
     .sort([[sort_by, sort_order]])
     .limit(total);
 
-    if(!orders){
-        return res.status(400).json({error: "Error"})
-    }
+  if (!orders) {
+    return res.status(400).json({ error: "Error" });
+  }
 
-    return res.send(orders)
-}
+  return res.send(orders);
+};
 
-
-
-exports.getOrderByUser = async(req,res) => {
-    const { sortBy, limit, sortOrder } = req.query;
+exports.getOrderByUser = async (req, res) => {
+  const { sortBy, limit, sortOrder } = req.query;
 
   const sort_by = sortBy ? sortBy : "createdAt";
   const total = limit ? limit : 100000000;
   const sort_order = sortOrder ? sortOrder : "asc";
 
-    const {userId} = req.params 
+  const { userId } = req.params;
 
-    const orders = await OrderModel.find({user: userId})
-    .select('-user')
+  const orders = await OrderModel.find({ user: userId })
+    .select("-user")
     // .populate('user', 'userDetail email')
-    .populate('product')
+    .populate("product")
     .sort([[sort_by, sort_order]])
     .limit(total);
 
-    return res.send(orders)
-}
+  return res.send(orders);
+};
+
+exports.deleteOrder = async (req, res) => {
+  const { orderId } = req.params;
+
+  const order = await OrderModel.findByIdAndDelete(orderId);
+
+  if (!mongoose.Types.ObjectId.isValid(orderId)) {
+    return res.status(400).json({ error: "Invalid ID" });
+  }
+
+  if (!order) {
+    return res.status(400).json({ error: "Order not found" });
+  }
+  return res.status(200).json({ message: "Order deleted" });
+};
--- backend/Route/orderRoute.js
+++ backend/Route/orderRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
-const { createOrder, getAllOrders, getOrderByUser } = require("../Controller/orderController");
+const { createOrder, getAllOrders, getOrderByUser, deleteOrder } = require("../Controller/orderController");
 const router = express.Router();
 
 router.post("/create-order", createOrder);
 router.get("/get-order", getAllOrders);
 router.get("/get-order/:userId", getOrderByUser);
+router.delete("/delete-order/:orderId", deleteOrder);
+
 
 module.exports = router;
