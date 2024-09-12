00cabfe872ece30fbcef4a00bf64fba3a9253f03
prakashmul <23prakashmul@gmail.com>
2024-09-12 19:52:45 +0545

    logout

--- frontend/src/layout/side-bar.tsx
+++ frontend/src/layout/side-bar.tsx
@@ -1,30 +1,36 @@
-
-
 import {
   Home,
-  LineChart,
   Package,
   Package2,
   ShoppingCart,
   Users,
 } from "lucide-react"
-import { Link } from "react-router-dom"
-
+import { Link, useNavigate } from "react-router-dom"
+import Button from "../component/reusable/button/button"
+import Cookies from "js-cookie"
 
 interface Props {
-  role?: String
+  role?: string
 }
 
 
 export function SideBar({ role }: Props) {
+  const navigate = useNavigate()
+  const logOut = () => {
+    Cookies.remove("userId")
+    Cookies.remove("role")
+    Cookies.remove("accessToken")
+    navigate('/login')
+  }
   return (
     <div className="fixed w-[240px] bg-white h-screen">
-      <div className="hidden border-r bg-muted/40 md:block h-full">
+
+      <div className="hidden border-r md:block h-full">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link to="/" className="flex items-center gap-2 font-semibold">
               <Package2 className="h-6 w-6" />
-              <span className="">E-commerce</span>
+              <span className="">E-Commerce</span>
             </Link>
           </div>
           <div className="flex-1">
@@ -43,37 +49,36 @@ export function SideBar({ role }: Props) {
                     to="/dashboard/category"
                     className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
                   >
-                    <ShoppingCart className="h-4 w-4" />
+                    <Package className="h-4 w-4" />
                     Category
                   </Link>
                   <Link
-                    to="dashboard/products"
+                    to="/dashboard/products"
                     className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
                   >
                     <Package className="h-4 w-4" />
                     Products{" "}
                   </Link>
                   <Link
-                    to="dashboard/customers"
+                    to="/dashboard/customers"
                     className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
                   >
                     <Users className="h-4 w-4" />
-                    Users
+                    Customers
                   </Link>
                   <Link
                     to="/dashboard/orders"
                     className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
                   >
-                    <LineChart className="h-4 w-4" />
+                    <ShoppingCart className="h-4 w-4" />
                     Orders
                   </Link>
                 </>
               }
-
               {
-                role === "user" && 
+                role === "user" &&
                 <>
-                 <Link
+                  <Link
                     to="/user-dashboard"
                     className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
                   >
@@ -98,8 +103,22 @@ export function SideBar({ role }: Props) {
               }
             </nav>
           </div>
+          <div className="mb-10 p-4 w-full">
+
+            <Button
+              buttonType={"button"}
+              className="w-full"
+              onClick={logOut}
+              buttonColor={{
+                secondary: true,
+              }}>
+              Logout
+            </Button>
+          </div>
         </div>
+
       </div>
+
     </div>
   )
 }
